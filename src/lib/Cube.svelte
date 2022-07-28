5daa98860cbcc2cdfcb6b2957c31468f4b081c4f
tasmankingsley <tasman.kingsley@gmail.com>
2022-07-28 13:46:42 +0900

    update

--- src/lib/Cube.svelte
+++ src/lib/Cube.svelte
@@ -1,58 +1,57 @@
 <script>
-    import {onMount} from 'svelte';
-    import * as THREE from 'three';
-	
-	let container;
+import {onMount} from 'svelte';
+import * as THREE from 'three';
 
-    onMount(() => {
-        const scene = new THREE.Scene();
-        scene.background = new THREE.Color( 0x1e1f29 );
+let container;
 
-        const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
+onMount(() => {
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x1e1f29 );
 
-        const renderer = new THREE.WebGLRenderer( { antialias: true } );
-        renderer.setSize( window.innerWidth, window.innerHeight);
-        
-        document.body.appendChild( renderer.domElement );
+    const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-        const geometry = new THREE.BoxGeometry();
-        const edges = new THREE.EdgesGeometry( geometry );
-        const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xc6afff } ) );
-        scene.add( line );
-        const material = new THREE.MeshBasicMaterial( { color: 0xc68fff } );
-        const cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
+    const renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight);
+    
+    document.body.appendChild( renderer.domElement );
 
-        camera.position.z = 2;
-        camera.position.y = -0.2;
+    const geometry = new THREE.BoxGeometry();
+    const edges = new THREE.EdgesGeometry( geometry );
+    const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xc6afff } ) );
+    scene.add( line );
+    const material = new THREE.MeshBasicMaterial( { color: 0xc68fff } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
 
-        const animate = function () {
-            requestAnimationFrame( animate );
+    camera.position.z = 2;
+    camera.position.y = -0.2;
 
-            cube.rotation.x += 0.02;
-            cube.rotation.y += 0.02;
+    const animate = function () {
+        requestAnimationFrame( animate );
 
-            line.rotation.x += -0.02;
-            line.rotation.y += -0.02;
+        cube.rotation.x += 0.02;
+        cube.rotation.y += 0.02;
 
-            renderer.render( scene, camera );
-        };
+        line.rotation.x += -0.02;
+        line.rotation.y += -0.02;
 
-        animate();
+        renderer.render( scene, camera );
+    };
 
-        function resize() {
-            var factor = 1; // percentage of the screen
-            var w = window.innerWidth * factor;
-            var h = window.innerHeight * factor;
-            renderer.setSize(w, h);
-            camera.aspect = w / h;
-            camera.updateProjectionMatrix();
-        }; 
+    animate();
 
-        window.addEventListener("resize", resize);        
+    function resize() {
+        var factor = 1; // percentage of the screen
+        var w = window.innerWidth * factor;
+        var h = window.innerHeight * factor;
+        renderer.setSize(w, h);
+        camera.aspect = w / h;
+        camera.updateProjectionMatrix();
+    }; 
 
-    });
+    window.addEventListener("resize", resize);        
 
+});
 
 </script>
 
